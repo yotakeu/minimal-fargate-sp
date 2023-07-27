f1b1c77983915a5f397d50a60b8dbe6997782dbf
yotakeu <108512858+yotakeu@users.noreply.github.com>
2023-07-27 10:44:13 +0900

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -4,15 +4,15 @@ variable "region" {
 }
 
 variable "vpc_id" {
-  default = "vpc-02c8b4517237cc1c4"
+  default = "vpc-xxxxxxxxxxxx"
 }
 
 variable "subnet_a_id" {
-  default = "subnet-0f464b3ab09d20015"
+  default = "subnet-xxxxxxxxxxxx"
 }
 
 variable "subnet_b_id" {
-  default = "subnet-040a8eb342a9a7ec2"
+  default = "subnet-xxxxxxxxxxxx"
 }
 
 # Sysdig Access key
